e625307b302b882f7b9e67b48d12660de27b2ca0
Rob Ballantyne <raballantyne@gmail.com>
2019-07-02 22:09:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,7 @@ end
 
 def monopoly_with_second_tier
 		monopoly = {
-	  :railroads => { :pieces => 4, :names => {}, :rent_in_dollars => {},  :one_piece_owned => {} }
+	  :railroads => { :pieces => 4, :names => {}, :rent_in_dollars => {}}
 	}
 
 end
